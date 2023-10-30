8dad7f8991b97e96572f3fa171bd4b8e629a8e21
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-10-30 08:14:25 -0600

    added form to create a new url and added the route for it

--- express_server.js
+++ express_server.js
@@ -9,13 +9,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
