10962fc4069fc194a8711d09a41326aa3b035e9b
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-10-31 07:45:36 -0600

    installed cookie parser and passed username to header for display when user is logged in

--- express_server.js
+++ express_server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 const generateRandomString = function() {
   let randomString = '';
@@ -24,7 +27,7 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -37,11 +40,12 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
